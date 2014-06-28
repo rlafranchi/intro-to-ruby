07dce52b3286cac94d618e36b4aee9782af6da76
rlafranchi <rlafranchi@serenethemes.com>
2014-06-28 10:40:16 -0600

    lesson3 quiz

---
+++ lesson3/quiz.rb
@@ -0,0 +1,41 @@
+#Please review this quiz, which covers lesson 3 materials.
+#
+
+# What is HTML? What is CSS? What is Javascript?
+# HTML stands for HyperText Markup Language which is basically what renders content on a web page.  CSS is used to syle HTML and javascript is used to dynamically change CSS, HTML or other elements after a page is loaded.
+
+# What are the major parts of an HTTP request?
+# GET
+
+# What are the major parts of an HTTP response?
+# POST
+
+# How do you submit an HTTP POST request, with a "username" attribute set to "bob"? What if we wanted a GET request instead?
+# POST Create a form with an input element and a name attribute of username and a value of bob.
+# GET would use the url and append ?username=bob
+
+# Why is it important for us, as web developers, to understand that HTTP is a "stateless" protocol?
+# 
+
+# If the internet is just HTTP requests/responses, why do we only use browsers to interface with web applications? Are there any other options?
+# Yes
+
+# What is MVC, and why is it important?
+# Model-View-Controller is important for rendering content
+
+# The below questions are about Sinatra:
+
+# At a high level, how are requests processed?
+# using the get method
+
+# In the controller/action, what's the difference between rendering and redirecting?
+# redirecting will redirect to another page and rendering will display
+
+# In the ERB view template, how do you show dynamic content?
+# using ruby commands wrapped in <% %>
+
+# Given what you know about ERB templates, when do you suppose the ERB template is turned into HTML?
+# View
+
+# What's the role of instance variables in Sinatra?
+# To be able to use variables globally
